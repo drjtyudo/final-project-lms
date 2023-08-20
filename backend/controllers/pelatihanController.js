0ae71645f8f786f09b76bebe0f4de202ee9e480a
iqblfrdsyh <iqblfrdsyh@gmail.com>
2023-08-20 17:22:31 +0700

    add include model rating in pelatihan

--- backend/controllers/pelatihanController.js
+++ backend/controllers/pelatihanController.js
@@ -1,13 +1,33 @@
-const { Pelatihan, Kategori } = require("../helper/relation");
+const { Pelatihan, Kategori, Rating } = require("../helper/relation");
 const path = require("path");
 const crypto = require("crypto");
 const fs = require("fs");
+const { getRating } = require("./ratingController");
 
 exports.getPelatihan = async (req, res) => {
   try {
+    const averageRating = await getRating()
     const response = await Pelatihan.findAll({
-      include: { model: Kategori, as: "Kategoris" },
+      include: [
+        {
+          model: Kategori,
+          as: "Kategoris",
+          attributes: [
+            "id",
+            "kategori",
+            "deskripsi",
+            "image_logo",
+            "url_image",
+          ],
+        },
+        {
+          model: Rating,
+          as: "Ratings",
+          attributes: ["id", "rating", "id_user", averageRating],
+        },
+      ],
     });
+    
     res.status(200).json({ msg: "success", response });
   } catch (error) {
     console.log(error.message);
@@ -68,7 +88,6 @@ exports.createPelatihan = async (req, res) => {
 
 exports.updatePelatihan = async (req, res) => {};
 
-
 exports.deletePelatihan = async (req, res) => {
   try {
     const pelatihan = await Pelatihan.findOne({
@@ -81,19 +100,18 @@ exports.deletePelatihan = async (req, res) => {
       return res.status(404).json({ msg: "pelatihan tidak ditemukan" });
     }
 
-      // Hapus file lama jika ada
-      if (pelatihan.image) {
-        const filePath = path.join(
-          __dirname,
-          "../public/assets/pelatihan-image",
-          pelatihan.image
-        );
+    // Hapus file lama jika ada
+    if (pelatihan.image) {
+      const filePath = path.join(
+        __dirname,
+        "../public/assets/pelatihan-image",
+        pelatihan.image
+      );
 
-        if (fs.existsSync(filePath)) {
-          fs.unlinkSync(filePath);
-        }
+      if (fs.existsSync(filePath)) {
+        fs.unlinkSync(filePath);
       }
-
+    }
 
     await pelatihan.destroy({
       where: {
