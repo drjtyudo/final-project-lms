63ae932da5ccfac890ec74012348a6c05d7d330f
Zackisaeful <zackisaefulbahri671@gmail.com>
2023-08-22 01:07:45 +0700

    revisi

---
+++ tes.js
@@ -0,0 +1,8 @@
+// var angka1 = 2.883333;
+var angka1 = 2;
+var angkaBulat1 = Math.floor(angka1 * 10) / 10; 
+console.log(angkaBulat1); // Output: 2.8
+
+var angka2 = 2.883333;
+var angkaBulat2 = parseFloat(angka2.toFixed(1)); 
+console.log(angkaBulat2); // Output: 2.9
