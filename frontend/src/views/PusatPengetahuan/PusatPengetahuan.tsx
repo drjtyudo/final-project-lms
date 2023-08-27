1896b9077f8a05550c32cfc88cb0808babb941a0
sabilah Mudrikah <sabilahmudrikah@gmail.com>
2023-08-27 11:03:40 +0700

    fix(sidebarFilter) fix styling sidebarfilter

--- frontend/src/views/PusatPengetahuan/PusatPengetahuan.tsx
+++ frontend/src/views/PusatPengetahuan/PusatPengetahuan.tsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import SidebarFilter from 'components/SidebarFilter/SidebarFilter'
+import NavigationBar from 'layouts/containers/Public/Navbar'
+import React, { useState } from 'react'
 
 function PusatPengetahuan() {
+  const categories = [
+    'Kategori 1',
+    'Kategori 2',
+    'Kategori 3',
+    'Kategori 4',
+    'Kategori 5',
+  ]
+
+  const [filteredCategories, setFilteredCategories] = useState<string[]>([])
+
+  const handleCategoryChange = (selectedCategories: string[]) => {
+    setFilteredCategories(selectedCategories)
+    // Di sini Anda dapat mengupdate data atau melakukan filter data dengan kategori yang dipilih.
+  }
+
   return (
-    <div>
-      haloo
-    </div>
+    <>
+      <NavigationBar />
+      <div className="grid grid-cols-2 gap-4">
+        <SidebarFilter
+          categories={categories}
+          onCategoryChange={handleCategoryChange}
+        />
+      </div>
+    </>
   )
 }
 
